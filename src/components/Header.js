6009662fe1fa448c16d2f146d1fa5d64e21dc549
mennatallah222 <mennatallahahmed892@gmail.com>
2024-09-25 21:27:33 +0300

    adjusted fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return (
     <nav className="nav-links">
-        <div>Mennatallah Ahmed</div>
+        <div style={{fontFamily:"Pacifico"}}>M</div>
         <div style={{display:"flex", gap:"20px"}}>
             <Link to={'/projects'}>Projects</Link>
             <Link to={'/'}>About</Link>
